e3a36eb15252141144ce48fca6d0badd25910ee6
王嘉睿 <99505726+cnbilinyj@users.noreply.github.com>
2022-12-21 14:05:46 +0800

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+let toggleBtn = document.getElementById("toggle-btn");
+let body = document.body;
+let darkMode = localStorage.getItem("dark-mode");
 
+const enableDarkmode = () =>{
+    toggleBtn.classList.replace("fa-sun", "fa-moon");
+    body.classList.add("dark");
+    localStorage.setItem("dark-mode", "")
+}
+
+const disableDarkmode = () =>{
+    toggleBtn.classList.replace("fa-moon", "fa-sun");
+    body.classList.remove("dark");
+    localStorage.setItem("dark-mode", "disable");
+}
+
+if(darkMode === "enable"){
+    enableDarkmode();
+}
+
+toggleBtn.onclick = (e) =>{
+    darkMode = localStorage.getItem("dark-mode");
+    if(darkMode === "disable"){
+        enableDarkmode();
+    }else{
+        disableDarkmode();
+    }
+}
