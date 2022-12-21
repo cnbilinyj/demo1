62a279f74c2b85c818f8e8edbd7e9e8e47b09d38
王嘉睿 <99505726+cnbilinyj@users.noreply.github.com>
2022-12-21 14:19:31 +0800

    update script.js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,26 @@ let body = document.body;
 let darkMode = localStorage.getItem("dark-mode");
 
 const enableDarkmode = () =>{
-    toggleBtn.classList.replace("fa-sun", "fa-moon");
-    body.classList.add("dark");
-    localStorage.setItem("dark-mode", "enable")
+	toggleBtn.classList.replace("fa-sun", "fa-moon");
+	body.classList.add("dark");
+	localStorage.setItem("dark-mode", "enable")
 }
 
 const disableDarkmode = () =>{
-    toggleBtn.classList.replace("fa-moon", "fa-sun");
-    body.classList.remove("dark");
-    localStorage.setItem("dark-mode", "disable");
+	toggleBtn.classList.replace("fa-moon", "fa-sun");
+	body.classList.remove("dark");
+	localStorage.setItem("dark-mode", "disable");
 }
 
 if(darkMode === "enable"){
-    enableDarkmode();
+	enableDarkmode();
 }
 
 toggleBtn.onclick = (e) =>{
-    darkMode = localStorage.getItem("dark-mode");
-    if(darkMode === "disable"){
-        enableDarkmode();
-    }else{
-        disableDarkmode();
-    }
+	darkMode = localStorage.getItem("dark-mode");
+	if(darkMode === "disable"){
+		enableDarkmode();
+	}else{
+		disableDarkmode();
+	}
 }
